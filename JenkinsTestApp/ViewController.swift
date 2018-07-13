18bb20fb40125a4faaa4d5c62c1121e1667a14ea
MartinHegyi_Unicorn <martin.hegyi@unicorn.com>
2018-07-13 10:21:32 +0200

    Initial commit

---
+++ JenkinsTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JenkinsTestApp
+//
+//  Created by User on 13/07/2018.
+//  Copyright © 2018 unicorn systems a.s. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
